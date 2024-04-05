e42be4393d48a1fe9d25b6fb0301ed9cb5f0293b
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2024-04-05 23:30:35 +0700

    update: full source

--- app/src/main/java/com/hoangtien2k3/themoviedb/MovaApp.kt
+++ app/src/main/java/com/hoangtien2k3/themoviedb/MovaApp.kt
@@ -16,16 +16,14 @@ class MovaApp : Application() {
     }
 
     private fun setupTimber() {
-
         val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
-            .showThreadInfo(true) // (Optional) Whether to show thread info or not. Default true
-            .methodCount(1) // (Optional) How many method line to show. Default 2
-            .methodOffset(5) // Set methodOffset to 5 in order to hide internal method calls
-            .tag("PRETTY_LOGGER") // To replace the default PRETTY_LOGGER tag with a dash (-).
+            .showThreadInfo(true)
+            .methodCount(1)
+            .methodOffset(5)
+            .tag("PRETTY_LOGGER")
             .build()
 
         Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
-
         Timber.plant(object : Timber.DebugTree() {
             override fun log(
                 priority: Int, tag: String?, message: String, t: Throwable?
